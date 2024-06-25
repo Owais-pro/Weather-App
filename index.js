86b423353af7a455c426520d07e9dc193ad92779
Rao-Owais <rao.owais2020@gmail.com>
2024-06-25 17:43:08 +0500

    completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Location = document.querySelector('#input');
 const div = document.querySelector('div');
 const Form = document.querySelector('form');
+const SearchResult = [];
 
 Form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -9,21 +10,30 @@ Form.addEventListener('submit', (event) => {
         .then((res) => {
             console.log(res.data);
             const Data = res.data;
-            div.innerHTML = `
-            <div id="inner-box" class="bg-white border shadow-lg w-50 mx-auto mt-5 rounded-lg p-4 mb-5">
-    <h1 class="h2 ">${Data.location.name} , ${Data.location.region}</h1>
-    <p class="text-light">${Data.location.localtime} , ${Data.location.country}</p>
-    <div class="mt-4 d-flex justify-content-between align-items-center mx-4 pb-4">
-        <h2 class="display-3 display-md-1 display-lg-1">${Data.current.temp_c}°C</h2>
-        <img class="w-25" src="${Data.current.condition.icon}" alt="weatherImg" />
-    </div>
-    <p class="h5">${Data.current.condition.text}</p>
-</div>
-
-            `
-
+            SearchResult.push(Data);
+            RenderResult();
+            
         })
         .catch((err) => {
             console.log(err);
         });
 });
+
+function RenderResult() {
+ div.innerHTML = ' ';
+ SearchResult.map((Data)=>{
+    div.innerHTML += `
+    <div id="inner-box" class="bg-white border shadow-lg w-50 mx-auto mt-5 rounded-lg p-4 mb-5">
+<h1 class="h2 ">${Data.location.name} , ${Data.location.region}</h1>
+<p class="text-light">${Data.location.localtime} , ${Data.location.country}</p>
+<div class="mt-4 d-flex justify-content-between align-items-center mx-4 pb-4">
+<h2 class="display-3 display-md-1 display-lg-1">${Data.current.temp_c}°C</h2>
+<img class="w-25" src="${Data.current.condition.icon}" alt="weatherImg" />
+</div>
+<p class="h5 text-light">${Data.current.condition.text}</p>
+</div>
+`
+
+ })
+    
+}
